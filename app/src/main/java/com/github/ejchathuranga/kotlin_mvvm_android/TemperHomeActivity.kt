0a67f7c251e600594cc7155b0fe4c2435dc78546
EJ <ejcindunil@gmail.com>
2022-03-18 17:52:30 +0530

    activity rename

--- app/src/main/java/com/github/ejchathuranga/kotlin_mvvm_android/TemperHomeActivity.kt
+++ app/src/main/java/com/github/ejchathuranga/kotlin_mvvm_android/TemperHomeActivity.kt
@@ -3,9 +3,9 @@ package com.github.ejchathuranga.kotlin_mvvm_android
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class TempworkHomeActivity : AppCompatActivity() {
+class TemperHomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.temper_activity_main)
     }
 }
