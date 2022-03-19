d5576ecac1164796ee71e86a9f40deeae0b31bd0
EJ <ejcindunil@gmail.com>
2022-03-19 09:23:41 +0530

    matched with response data on adapter

--- app/src/main/java/com/github/ejchathuranga/kotlin_mvvm_android/views/home/JobsAdapter.kt
+++ app/src/main/java/com/github/ejchathuranga/kotlin_mvvm_android/views/home/JobsAdapter.kt
@@ -1,5 +1,6 @@
 package com.github.ejchathuranga.kotlin_mvvm_android.views.home
 
+import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
@@ -8,7 +9,6 @@ import com.github.ejchathuranga.kotlin_mvvm_android.models.TemperJob
 import com.squareup.picasso.Picasso
 
 class JobsAdapter : RecyclerView.Adapter<JobsAdapter.JobViewHolder>() {
-
     private var dataList: ArrayList<TemperJob> = ArrayList<TemperJob>()
 
     fun setDataList(dataList: ArrayList<TemperJob>) {
@@ -34,9 +34,26 @@ class JobsAdapter : RecyclerView.Adapter<JobsAdapter.JobViewHolder>() {
 
     class JobViewHolder constructor(var binding: JobsViewHoldeBinding) :
         RecyclerView.ViewHolder(binding.root) {
+        private val TAG = "JobsAdapter"
+        private val curr: String = "€ "
+        private val dist: String = " KM"
+
+        @SuppressLint("SetTextI18n")
         fun bind(data: TemperJob) {
-            binding.tvName.text = data.job.project.client.name
-            binding.tvDesc.text = data.earnings_per_hour.currency
+            binding.tvClientName.text = data.job.project.client.name
+
+            if (data.earnings_per_hour.currency.equals("EUR")) {
+                binding.tvHourlyRate.text = curr + data.earnings_per_hour.amount
+            } else {
+                TODO()
+            }
+
+            binding.tvCategory.text = data.job.category.name
+            binding.tvDistance.text = data.distance.toInt().toString() + dist
+
+            /*
+            * Date time conversion need to be done
+            * */
 
             val url = data.job.project.client.links.hero_image;
 
